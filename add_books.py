5b26888d1d7205aa5bb442800283fc5b6326817b
shaikhahan03 <shaikhahan03@gmail.com>
2026-04-27 00:36:45 +0530

    Implement add_books function to add a book

--- add_books.py
+++ add_books.py
@@ -1,1 +1,7 @@
+from utils import books
 
+
+def add_books():
+    book_name = input("Enter the name of the book: ").strip().upper()
+    books.append(book_name)
+    print(f"'{book_name}' has been added to the library.")
