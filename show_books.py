5fcc1e716a84dee7a00c190514eb24871d7b4039
shaikhahan03 <shaikhahan03@gmail.com>
2026-04-27 00:35:54 +0530

    Import books and implement show_books function

--- show_books.py
+++ show_books.py
@@ -1,1 +1,10 @@
+from utils import books
 
+
+def show_books():
+    if len(books) == 0:
+        print("No books in the library.")
+    else:
+        print("Available Books in the library:")
+        for book in books:
+            print(f"- {book}")
